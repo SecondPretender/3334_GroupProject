26869a2f8afb76f83cfc22c6235b0d9db8103a7f
Peter <peterm03cg@gmail.com>
2023-04-30 20:00:27 -0500

    added more images and fixed piece file

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char ** argv)
     //TEMP: CREATE A MASSIVE 2D ARRAY OURSELVES
     int vicState = 0;
 
-    file.open("500x500Walter.png.txt");
+    file.open("boothiful.png.txt");
     int height = 0;
     int width = 0;
     int xOff = 500;
